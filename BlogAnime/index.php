9990e57f7363ef2e6d314a3c1c32008e2db60770
LAVAUD Loe <Loe.LAVAUD@cpnv.ch>
2023-02-03 14:19:10 +0100

    MVC complet and start of the code and
    the MVC organisation is terminate and the code is started with juste the index start the home page passing by the controller

---
+++ BlogAnime/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'controller/navigation.php';
+
+if (isset($_GET['action'])){
+    $action = $_GET['action'];
+    switch ($action){
+        case "home" :
+            home();
+            break;
+        default :
+            lost();
+    }
+}
+else{
+    home();
+}
