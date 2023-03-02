9cb9ebfbb97065cf42e421458cdb2964c5a34f8c
leo-jaquier <leo.jaquier@cpnv.ch>
2023-03-02 16:36:50 +0100

    fix problem of the last push

--- BlogAnime/view/gabarit.php
+++ BlogAnime/view/gabarit.php
@@ -6,16 +6,16 @@
     <link rel="stylesheet" type="text/css" href="view/css/style.css">
 </head>
 <body>
-<<<<<<< HEAD
 <!--Header-->
 <header id="topbar">
     <a href="index.php" class="Image-header"><img class="Logo" src="view/image/Logo_BlogAnime.png" alt="logo"></a>
     <nav class="navbar">
+
         <div class="ulNavbar">
             <a class="navlink" href="index.php?action=home">Accueil</a>
             <a class="navlink" href="index.php?action=Blog">Blog</a>
             <a class="navlink" href="index.php?action=AboutUs">A propos de nous</a>
-            <a id="Login" href="#">Login </a>
+            <a id="Login" href="#">Login</a>
         </div>
         <div class="navbarbtn" onclick="displayNavbar(this)">
             <div class="barbtn1"></div>
@@ -23,31 +23,13 @@
             <div class="barbtn3"></div>
         </div>
     </nav>
-=======
-
-    <div id="Box1">
-             <a href="index.php" class="Logo"><img class="Image-header" src="../view/image/Logo_BlogAnime.png"></a>
-
-         <nav class="menu">
-             <ul class="main_menu">
-                 <li><a class="navlink" href="index.php">Accueil</a></li>
-                 <li><a class="navlink" href="index.php?action=Blog">Blog</a></li>
-                 <li><a class="navlink" href="index.php?action=AboutUs">A propos de nous</a></li>
-             </ul>
-         </nav>
-
-         <div id="Login">
-             <a id="LoginText" href="index.php?action=Login">Login</a>
-         </div>
-     </div>
-
-    <!-- Header Mobile -->
-
-
->>>>>>> ce42fe927ab9f9a5e4d50c2ead63e8c08790920c
 </header>
 
+
+
 <?= $content; ?>
+
+
 
 <!--Footer-->
 <footer>
@@ -56,7 +38,6 @@
             <img class="Image-header" src="view/image/Logo_BlogAnime.png">
             <br>
             <br>
-
             <div id="form">
                 <label for="fMail"></label>
                 <input type="text" id="fMail" name="newsletter" placeholder="Newsletter ">
@@ -82,10 +63,10 @@
                 </tr>
                 <tr>
                     <td>
-                    <img class="logo" id="instagram" src="view/image/instagram-logo.png">
-                    <img class="logo" id="twitter" src="view/image/twitter-logo.jpg">
-                    <img class="logo" id="reddit" src="view/image/reddit-logo.jpg">
-                    <img class="logo" id="facebook" src="view/image/facebook-logo.jpg">
+                    <img class="Image-header" id="instagram" src="view/image/instagram-logo.png">
+                    <img class="Image-header" id="twitter" src="view/image/twitter-logo.jpg">
+                    <img class="Image-header" id="reddit" src="view/image/reddit-logo.jpg">
+                    <img class="Image-header" id="facebook" src="view/image/facebook-logo.jpg">
                     </td>
                 </tr>
             </table>
@@ -93,7 +74,6 @@
     </div>
 </footer>
 </body>
-<<<<<<< HEAD
 <script>
     function displayNavbar(btnIcon) {
         btnIcon.classList.toggle("change");
@@ -106,5 +86,4 @@
         //}
     }
 </script>
-=======
->>>>>>> ce42fe927ab9f9a5e4d50c2ead63e8c08790920c
+
