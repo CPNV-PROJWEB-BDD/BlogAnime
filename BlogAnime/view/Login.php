1a6f430ce57b634dfdeb3276a9800874ecf9a33f
LAVAUD Loe <Loe.LAVAUD@cpnv.ch>
2023-03-03 14:36:17 +0100

    front Login
    start of the html css of the login page

--- BlogAnime/view/Login.php
+++ BlogAnime/view/Login.php
@@ -13,18 +13,42 @@ $title = "BlogAnime";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <link rel="stylesheet" type="text/css" href="view/css/Login.css">
 </head>
 <body>
+<section class="bgwhite p-t-66 p-b-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 p-b-30">
+                <form class="leave-comment">
+                    <div class="bo4 size15 m-b-20">
+                        <input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="ArticleCode" placeholder="Code de l'article">
+                    </div>
 
+                    <div class="bo4 size15 m-b-20">
+                        <input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="ArticleBrand" placeholder="Marque de l'article">
+                    </div>
+
+                    <input type="submit" value="Se connecter" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
 
 </body>
 
 
+
+
 <?php
 
+$fileFullPath = "";//le chemin complet jusqu'au fichier
+$TextToWrite = "";
+$fileName = "";
+
 $fileFullPath = setFullPath("LoginName".".json");
-$TextToWrite = '{ <br>"Pseudo":"",<br>"Password":"" }</t> ';
+$TextToWrite = '{ <br></t>"Pseudo":"$Pseudo",<br></t>"Password":"$password" }<br><br> ';
 
 writeMsgInFile($fileFullPath, $TextToWrite);
 
