4925c47c421886761d97bd20d96fa2a1d2226620
Diogo-Oliveira-Soares <99669006+Diogo-Oliveira-Soares@users.noreply.github.com>
2023-03-17 15:00:03 +0100

    Blog page starting now

--- BlogAnime/view/blog.php
+++ BlogAnime/view/blog.php
@@ -9,8 +9,26 @@
 ob_start();
 $title = "Blog";
 ?>
+<!DOCTYPE html>
+    <html lang="en">
+<section>
+    <link rel="stylesheet" type="text/css" href="../view/css/blog.css">
+</section>
 
-
+<section>
+<div class="Block-blog">
+<?php for ($i = 0; $i <=6; $i++) : ?>
+    <div id="box3">
+        <div class="blogPost">
+            <img class="imgPost"src="../view/image/testImg.png" alt="test">
+            <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+            </article>
+            <a class="readMore" href="#0" >Read More </a>
+        </div>
+    </div>
+<?php endfor; ?>
+</div>
+</section>
 
 <?php
 $content = ob_get_clean();
