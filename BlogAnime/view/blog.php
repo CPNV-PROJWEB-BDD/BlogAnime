5626ebf0dc7250f099fc36b5c396e39cbd33ddbb
Diogo-Oliveira-Soares <99669006+Diogo-Oliveira-Soares@users.noreply.github.com>
2023-03-22 14:09:29 +0100

    Blog page V2

--- BlogAnime/view/blog.php
+++ BlogAnime/view/blog.php
@@ -9,25 +9,55 @@
 ob_start();
 $title = "Blog";
 ?>
-<!DOCTYPE html>
+    <!DOCTYPE html>
     <html lang="en">
 <section>
     <link rel="stylesheet" type="text/css" href="../view/css/blog.css">
 </section>
 
-<section>
-<div class="Block-blog">
-<?php for ($i = 0; $i <=6; $i++) : ?>
-    <div id="box3">
-        <div class="blogPost">
-            <img class="imgPost"src="../view/image/testImg.png" alt="test">
-            <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
-            </article>
-            <a class="readMore" href="#0" >Read More </a>
+<section class="Block-blog">
+        <div id="margin">
+            <?php for ($i = 0; $i < 3; $i++) : ?>
+                <div id="box3">
+                    <div class="blogPost">
+                        <img class="imgPost" src="../view/image/testImg.png" alt="test">
+                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
+                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
+                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        </article>
+                        <a class="readMore" href="#0">Read More </a>
+                    </div>
+                </div>
+            <?php endfor; ?>
         </div>
-    </div>
-<?php endfor; ?>
-</div>
+        <div id="margin">
+            <?php for ($i = 0; $i < 3; $i++) : ?>
+                <div id="box3">
+                    <div class="blogPost">
+                        <img class="imgPost" src="../view/image/testImg.png" alt="test">
+                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
+                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
+                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        </article>
+                        <a class="readMore" href="#0">Read More </a>
+                    </div>
+                </div>
+            <?php endfor; ?>
+        </div>
+        <div id="margin">
+            <?php for ($i = 0; $i < 3; $i++) : ?>
+                <div id="box3">
+                    <div class="blogPost">
+                        <img class="imgPost" src="../view/image/testImg.png" alt="test">
+                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
+                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
+                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        </article>
+                        <a class="readMore" href="#0">Read More </a>
+                    </div>
+                </div>
+            <?php endfor; ?>
+        </div>
 </section>
 
 <?php
