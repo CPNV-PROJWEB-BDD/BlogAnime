f9cec37fc7d719bedadfe2d095e91cdfaf9b7f66
Diogo-Oliveira-Soares <99669006+Diogo-Oliveira-Soares@users.noreply.github.com>
2023-03-23 15:41:15 +0100

    Home version fix

--- BlogAnime/view/home.php
+++ BlogAnime/view/home.php
@@ -7,37 +7,40 @@ $title = "BlogAnime";
     <link rel="stylesheet" type="text/css" href="view/css/home.css">
 </section>
 
-<div class="Box">
-    <div class="welcome">
-
-        <a>Bonjour !<br> et bienvenue sur <br> notre site de blog d'anime</a>
-
+<section>
+<div class="Border-page">
+    <div class="Box">
+        <div class="welcome">
+            <a>Bonjour !<br> et bienvenue sur <br> notre site de blog d'anime</a>
+        </div>
+        <img class="Welcome-Image" src="view/image/HomePicture.png">
     </div>
-    <img class="Welcome-Image" src="view/image/HomePicture.png">
-</div>
 
-<div class="Box">
-    <div class="InfoContent">
+    <div class="Box">
+        <div class="InfoContent">
             <a>notre site propose un blog contenant information et details sur les personnages d'animées</a>
+        </div>
     </div>
-</div>
 
-<div class="Box">
-    <div class="Welcome-Image">
-        <a>images decrivant un articles</a>
-    </div>
-    <div class="Enjoy">
-        <a>Amusez vous à explorer notre site !<br>Et découvrez tout ce que vous chercher<br> sur vos personnage préféré !!!</a>
-    </div>
-</div>
+    <div class="Box">
+        <div class="Welcome-Image">
+            <a>images decrivant un articles</a>
+        </div>
+        <div class="Enjoy">
+            <a>Amusez vous à explorer notre site !<br>Et découvrez tout ce que vous chercher<br> sur vos personnage
+            préféré !!!</a>
+        </div>
+   </div>
 
-<div class="Box">
-    <img src="../view/image/JoinBanner.png" id="Joinbanner">
-    <div id="Join">
-        <a href="index.php?action=register"><button class="recBtn" role="button">Click to Join us !</button></a>
-    </div>
+   <div class="Box-image">
+        <div id="Join">
+            <a href="index.php?action=register">
+                <button class="recBtn" role="button">Click to Join us !</button>
+            </a>
+        </div>
+  </div>
 </div>
-
+</section>
 
 <?php
 $content = ob_get_clean();
