106e4bb9b2a6527d957b6f4f083533b79b56e5d8
leo-jaquier <leo.jaquier@cpnv.ch>
2023-03-24 14:38:17 +0100

    fixed Desktop footer mobile footer done. top bar have an issue need to be repaired

--- BlogAnime/view/gabarit.php
+++ BlogAnime/view/gabarit.php
@@ -12,7 +12,7 @@
 <header id="topbar">
 
     <nav class="navbar">
-        <a href="index.php" class="Image-header"><img class="Logo" src="view/image/Logo_BlogAnime.png" alt="logo"></a>
+        <a href="index.php" class="Logo-header"><img class="Logo" src="view/image/Logo_BlogAnime.png" alt="logo"></a>
         <div class="mobileNavbar">
             <div class="navbarbtn" onclick="displayNavbar(this)">
                 <div class="barbtn1"></div>
@@ -36,40 +36,50 @@
 
 <!--Footer-->
 <footer id="footer">
-    <div>
-
+    <div id="FooterInformation" >
+        <div id="footerform">
+            <img id="Logo-footer" src="view/image/Logo_BlogAnime.png">
+            <div id="form">
+                <label for="fMail"></label>
+                <input type="text" id="fMail" name="newsletter" placeholder="Newsletter ">
+                <a type="submit" id="btnSubmit">></a>
+            </div>
+        </div>
         <div id="navigationalKeys">
             <table>
                 <tr>
                     <td><span><a href="index.php?action=home">Accueil</a></span></td>
-                    <td>Nous contacter</td>
+                    <td><a>contacter</a> </td>
                 </tr>
                 <tr>
-                    <td><span><a href="index.php?action=Blog">Blog</a></span></td>
-                    <td>aide</td>
+                    <td><span><a  href="index.php?action=Blog">Blog</a></span></td>
+                    <td><a>aide</a></td>
                 </tr>
                 <tr>
-                    <td> <span><a href="index.php?action=AboutUs">A propos de nous</a></span></td>
+                    <td> <span><a  href="index.php?action=AboutUs">A propos de nous</a></span></td>
                 </tr>
                 <tr>
-                    <td><span><a href="index.php?action=login">Login</a></span></td>
+                    <td><span><a  href="index.php?action=login">Login</a></span></td>
+
                 </tr>
             </table>
+            <div id="contactDetails">
+                <a href="https://instagram.com/bloganime2023?igshid=ZDdkNTZiNTM=">
+                    <img class="Social-Footer" id="instagram" src="view/image/instagram-logo.png">
+                </a>
+                <a href="https://twitter.com/BlogAnime2023">
+                    <img class="Social-Footer" id="twitter" src="view/image/twitter-logo.jpg">
+                </a>
+                <a href="https://www.reddit.com/user/BlogAnime2023">
+                    <img class="Social-Footer" id="reddit" src="view/image/reddit-logo.jpg">
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=100090937893061">
+                    <img class="Social-Footer" id="facebook" src="view/image/facebook-logo.jpg">
+                </a>
+            </div>
+
         </div>
-        <div id="contactDetails">
-            <a href="https://instagram.com/bloganime2023?igshid=ZDdkNTZiNTM=">
-                <img class="Image-header" id="instagram" src="view/image/instagram-logo.png">
-            </a>
-            <a href="https://twitter.com/BlogAnime2023">
-                <img class="Image-header" id="twitter" src="view/image/twitter-logo.jpg">
-            </a>
-            <a href="https://www.reddit.com/user/BlogAnime2023">
-                <img class="Image-header" id="reddit" src="view/image/reddit-logo.jpg">
-            </a>
-            <a href="https://www.facebook.com/profile.php?id=100090937893061">
-                <img class="Image-header" id="facebook" src="view/image/facebook-logo.jpg">
-            </a>
-        </div>
+
     </div>
     <div id="legalContent">
         <span><a href="#">cookie</a></span>
