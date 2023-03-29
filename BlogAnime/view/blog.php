a59c43e35dd51ca8fb183a0463bb4c666fa57458
Diogo-Oliveira-Soares <99669006+Diogo-Oliveira-Soares@users.noreply.github.com>
2023-03-29 16:35:55 +0200

    Résumé page blog

--- BlogAnime/view/blog.php
+++ BlogAnime/view/blog.php
@@ -46,11 +46,8 @@ $title = "Blog";
                     <div class="blogPost">
                         <img class="imgPost" src="../view/image/Banner/Kirito.png" alt="test">
                         <article class="artPost"><p>Kirito du vrai nom Kazuto Kirigaya est un adolescent ayant été choisi comme un des 1000 beta testeur pour un nouveau jeu en
-                                réalité virtuelle : Sword Art Online. Après que le jeu soit sorti offici</p>
-                            <p>Kirito is a teenager who was chosen as one of 1,000 beta testers for a new virtual reality video game: Sword Art Online. After
-                                the game is released to the general public, he and the rest of the 10,000 players discover that they are unable to log out and are trapped in the
-                                simulation unless they manage to beat the game. In the anime adaptation, Kirito is voiced in Japanese by Yoshitsugu Matsuoka and in English by Bryce
-                                Papenbrook.</p>
+                                réalité virtuelle : Sword Art Online. Après que le jeu soit sorti officielement, lui ainsi qu'un total de 10'000 joueurs ont découverts qu'ils ne
+                                pouvaient plus quitter le jeu et qu'ils seraient coincé jusq'à terminer le jeu tout en sachant que si ils perdent c'est Game Over.</p>
                         </article>
                         <a class="readMore" href="index.php?action=article&name=Kazuto_Kirigaya">Read More </a>
                     </div>
@@ -62,9 +59,12 @@ $title = "Blog";
                 <div id="box3">
                     <div class="blogPost">
                         <img class="imgPost" src="../view/image/Banner/Hitori.png" alt="test">
-                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
-                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
-                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        <article class="artPost"><p>Hitori Gotoh est une jeune fille de nature solitaire ayant une grande passion pour la musique, en particulier la guitare. Sa
+                                timidité et son isolement l'empêchent de parler normalement avec d'autres personnes et elle n'est pas vraiment douée pour se lier d'amitié avec ses
+                                camarades.<br><br>
+                                Un jour, alors qu'elle regarde la télé avec son père, Hitori découvre qu'un des membres d'un groupe d'idol interviewé avait en quelque sorte la même
+                                vie qu'elle auparavant, mais que cela ne l'a pas empêché de devenir célèbre. La jeune fille décide alors d'emprunter la guitare de son père pour
+                                accéder aussi à la célébrité.</p>
                         </article>
                         <a class="readMore" href="index.php?action=article&name=Hitori_Gotō">Read More </a>
                     </div>
@@ -74,9 +74,9 @@ $title = "Blog";
                 <div id="box3">
                     <div class="blogPost">
                         <img class="imgPost" src="../view/image/Banner/Ichigo.png" alt="test">
-                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
-                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
-                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        <article class="artPost"><p>Ichigo Kurosaki est un Humain qui a la faculté exceptionnelle de distinguer les fantômes. Sa vie
+                                bascule lorsqu'il rencontre Rukia Kuchiki, qui lui transfère ses pouvoirs de Shinigami afin de sauver sa famille de l'attaque d'un Hollow. Ichigo est
+                                le protagoniste principal du manga Bleach.</p>
                         </article>
                         <a class="readMore" href="index.php?action=article&name=Ichigo_Kurosaki" >Read More </a>
                     </div>
@@ -86,9 +86,14 @@ $title = "Blog";
                 <div id="box3">
                     <div class="blogPost">
                         <img class="imgPost" src="../view/image/Banner/Naruto.png" alt="test">
-                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
-                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
-                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        <article class="artPost"><p>Naruto Uzumaki est un ninja du village de Konoha. Il devint le jinchûriki de Kyûbi le jour
+                                de sa naissance ce qui lui valut d'être rejeté par la plupart des habitants de Konoha durant toute son enfance. Après avoir rejoint l'Équipe 7,
+                                Naruto travailla dur pour obtenir la reconnaissance des villageois tout en poursuivant son rêve de devenir Hokage.
+                                <br>
+                                Dans les années qui suivirent,
+                                après de nombreuses péripéties et épreuves, il devint un ninja considéré comme un héros par les villageois et plus tard par le reste du monde. Il
+                                prouva rapidement être l'un des acteurs majeurs de la Quatrième Grande Guerre Shinobi, lui permettant de réaliser son rêve et de devenir le Septième
+                                Hokage.</p>
                         </article>
                         <a class="readMore" href="index.php?action=article&name=Naruto_Uzumaki">Read More </a>
                     </div>
@@ -100,9 +105,11 @@ $title = "Blog";
                 <div id="box3">
                     <div class="blogPost">
                         <img class="imgPost" src="../view/image/Banner/Lelouch.png" alt="test">
-                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
-                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
-                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        <article class="artPost"><p>Lelouch vi Britannia est le protagoniste et antihéros de Code Geass. Il est âgé de 17 ans.
+                                <br><br>
+                                Il est le fils du 98e empereur de Britannia, Charles zi Britannia et, de ce fait, le neuvième prince du
+                                Saint Empire de Britannia. Il est secrètement le chef de l'Ordre des Chevaliers Noirs, sous le pseudonyme de Zéro. Ainsi le masque et l'appellation
+                                lui permettent de cacher sa véritable identité aux yeux de tous.</p>
                         </article>
                         <a class="readMore" href="index.php?action=article&name=Lelouch_vi_Britannia">Read More </a>
                     </div>
@@ -112,9 +119,11 @@ $title = "Blog";
                 <div id="box3">
                     <div class="blogPost">
                         <img class="imgPost" src="../view/image/Banner/Eren_Jaeger.png" alt="test">
-                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
-                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
-                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        <article class="artPost"><p>Eren Jäger est un membre du Bataillon d'exploration et le protagoniste, puis l'antagoniste principal de la série.
+                                Il a vécu dans le District de Shiganshina avec ses parents et Mikasa Ackerman jusqu'à la chute du Mur Maria.
+                                <br><br>
+                                Au cours de l'incident, Eren assiste, impuissant, à la mort de sa mère par la main d'un Titan. Cet événement suscite chez Eren une haine intense
+                                envers les Titans, ce dernier voulant les éradiquer jusqu'au dernier sans savoir que son destin va bien basculé.</p>
                         </article>
                         <a class="readMore" href="index.php?action=article&name=Eren_Yeager">Read More </a>
                     </div>
@@ -124,9 +133,11 @@ $title = "Blog";
                 <div id="box3">
                     <div class="blogPost">
                         <img class="imgPost" src="../view/image/Banner/Deku.png" alt="test">
-                        <article class="artPost"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis
-                                minus delectus laborum magni quos corrupti doloribus aliquid corporis. Illum consectetur
-                                omnis cupiditate dolores, ullam ratione architecto ea odio beatae.</p>
+                        <article class="artPost"><p>Izuku Midoriya ou Deku , également appelé par sa mère Izuku-Might, est le personnage principal du manga My Hero Academia.
+                                Né Sans-Alter, il attirera l'attention d'All Might pour son héroïsme et deviendra l'héritier de son Alter : le One For All.
+                                <br><br>
+                                Poursuivant ses études au lycée Yuei, dans la seconde A, afin de devenir un héros professionnel, Izuku aura la lourde tâche en tant que neuvième
+                                détenteur du One for All de devenir le nouveau symbole de la paix après All Might.</p>
                         </article>
                         <a class="readMore" href="index.php?action=article&name=Izuku_Midoriya">Read More </a>
                     </div>
