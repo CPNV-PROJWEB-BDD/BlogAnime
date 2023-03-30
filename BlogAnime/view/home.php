d9e27a2b4dab039867a0db4fa32373bd56bdeb87
Diogo-Oliveira-Soares <99669006+Diogo-Oliveira-Soares@users.noreply.github.com>
2023-03-30 15:58:01 +0200

    Mobile versions + descriptions

--- BlogAnime/view/home.php
+++ BlogAnime/view/home.php
@@ -3,47 +3,48 @@ ob_start();
 $title = "BlogAnime";
 ?>
 
-<section>
-    <link rel="stylesheet" type="text/css" href="view/css/home.css">
-</section>
+    <section>
+        <link rel="stylesheet" type="text/css" href="view/css/home.css">
+    </section>
 
-<section>
-<div class="Border-page">
-    <div class="Box">
-        <img class="Welcome-Image" src="view/image/HomePicture.png">
-        <div class="welcome">
-            <h3>Bonjour !<br> et bienvenue sur <br> notre site de blog d'anime</h3>
-        </div>
-    </div>
+    <section>
+        <div class="Border-page">
+            <div class="Box">
+                <img class="Welcome-Image" src="view/image/HomePicture.png">
+                <div class="welcome">
+                    <h3>Bonjour !<br> et bienvenue sur <br> notre site de blog d'anime</h3>
+                </div>
+            </div>
 
-    <div class="Box">
-        <div class="InfoContent">
-            <h3>Notre blog sur les animes <br> propose des infos vérifiées par des fans <br> pour des fans</h3>
-        </div>
-        <div class="ImageContent">
-            <img id="ImageAnimeContent" src="../view/image/Image_home_Contenu.png">
-        </div>
-    </div>
+            <div class="Box">
+                <div class="InfoContent">
+                    <h3>Notre blog sur les animes <br> propose des infos vérifiées par des fans <br> pour des fans</h3>
+                </div>
+                <div class="ImageContent">
+                    <img id="ImageAnimeContent" src="../view/image/Image_home_Contenu.png">
+                </div>
+            </div>
 
-    <div class="Box">
-        <div class="Welcome-Image">
-            <img id="ImageAnimeContent" src='../view/image/ScreenArticle.PNG'>
-        </div>
-        <div class="Enjoy">
-            <h3>Amusez vous à explorer notre site !<br>Et découvrez tout ce que vous chercher<br> sur vos personnage
-            préféré !!!</h3>
-        </div>
-   </div>
+            <div class="Box">
+                <div class="Welcome-Image">
+                    <img id="ImageAnimeContent" src='../view/image/Banner/Eren_Jaeger.png'>
+                </div>
+                <div class="Enjoy">
+                    <h3>Amusez vous à explorer notre site !<br>Et découvrez tout ce que vous chercher<br> sur vos
+                        personnage
+                        préféré !!!</h3>
+                </div>
+            </div>
 
-   <div class="Box-image">
-        <div id="Join">
-            <a href="index.php?action=register">
-                <button class="recBtn" role="button">Click to Join us !</button>
-            </a>
+            <div class="Box-image">
+                <div id="Join">
+                    <a href="index.php?action=register">
+                        <button class="recBtn" role="button">Click to Join us !</button>
+                    </a>
+                </div>
+            </div>
         </div>
-  </div>
-</div>
-</section>
+    </section>
 
 <?php
 $content = ob_get_clean();
